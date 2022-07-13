d0e3fcaf23d13c16b009dfb0ffb761639b7401ee
ChadelD <104319964+ChadelD@users.noreply.github.com>
2022-07-13 13:01:50 -0400

    Home page changes

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -5,23 +5,35 @@ import "./Navbar.css";
 function Nav() {
     return (
             <nav className="nav-container">
-                <div className='flex-container'>
-                    <Link to="/" className="navbar-brand"><a href="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png"><img style={{ width: 125 }} src="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png" title="source: imgur.com" /></a></Link>
+                
+                <div className='greetings'>
+                    Greetings From Guru Guide !
                 </div>
+
                 <div className='search-container'>
+                
+                </div>
+                
+                <div className='navbuttons'>
+                    <a className="btn-btn-primary" href="/register" role="button">Sign Up </a>
+                    <a className="btn-btn-primary" href="/login" role="button">Log In </a>
+                    <a className="btn-btn-primary" href="/coachviewingpage" role="button">Find a Coach</a>
                 <form className="d-flex" role="search">
                         <input className="form-control me-4" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Submit</button>
                     </form>
                 </div>
+                
                 <div className="drop-container">
                         <button className="btn btn-light btn-lg dropdown-toggle drop-container" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             Account
                         </button>
                         <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                             <li><a className="dropdown-item" href="#"><Link to="/login" className="nav-link">Login</Link></a></li>
-                            <li><a className="dropdown-item" href="#"><Link to="/register" className="nav-link">Create Account</Link></a></li>
-                            <li><a className="dropdown-item" href="#"><Link to="/coachviewingpage" className="nav-link">Coaches Profiles</Link></a></li>
+                            <li><a className="dropdown-item" href="#"><Link to="/register" className="nav-link">Create Account as Customer</Link></a></li>
+                            <li><a className="dropdown-item" href="#"><Link to="/coachviewingpage" className="nav-link">Coaches View</Link></a></li>
+                            <li><a className="dropdown-item" href="#"><Link to="/" className="nav-link">Coaches Profile</Link></a></li>
+                            <li><a className="dropdown-item" href="#"><Link to="/" className="nav-link">Go Home</Link></a></li>
                             <li><a className="dropdown-item" href="#">Something else here</a></li>
                         </ul>
                     </div>
--- src/Routes/home/home.tsx
+++ src/Routes/home/home.tsx
@@ -1,67 +1,21 @@
 import React from 'react'
 import "./home.css";
-import "./jscript";
-import "./jscript2"
+
 
 function Home() {
   
-  function reveal2() {
-    var reveals = document.querySelectorAll(".reveal2");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible =0;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active2");
-      } else {
-        reveals[i].classList.remove("active2");
-      }
-    }
-  }
-  window.addEventListener("scroll", reveal2);
-
-  function reveal3() {
-    var reveals = document.querySelectorAll(".reveal3");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible =0;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active3");
-      } else {
-        reveals[i].classList.remove("active3");
-      }
-    }
-  }
-  window.addEventListener("scroll", reveal3);
-
-  
-
-  const test = () =>
-  {
-    window.scrollTo(0,1000)
-  }
-
-
-  
   return (
-    <div>
+  <div>
     <div className='column-layout'>
-      <p className='text-container'> Welcome To Guru Guide </p>
-        <div className='left-side-container'>
-        <a href="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png"><img style={{ width: 500 }} src="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png" title="source: imgur.com" /></a>
-        <a href="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png"><img style={{ width: 500 }} src="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png" title="source: imgur.com" /></a>
-        </div>
-        <div className="img-container">
-        </div>
-        <div className='right-side-container'>
+        <div className='container'>
+          <a href="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png"><img style={{ width: 500, height: 500 }} src="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png" title="source: imgur.com" /></a>
+          <a href="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png"><img style={{ width: 500, height: 500 }} src="https://cdn.discordapp.com/attachments/991749229183778817/994352372841189457/Screen_Shot_2022-07-06_at_4.50.03_PM.png" title="source: imgur.com" /></a>
         </div>
     </div>
-    <div className='bottom-container'>
-        <button type="button" className="btn btn-outline-primary btn-lg" onClick={test} style={{margin: 'auto'}}>About Us</button>
-        <button type="button" className="btn btn-outline-success btn-lg" style={{margin: 'auto'}}>I'm in Need of Guru Guidance</button>
-        <button type="button" className="btn btn-outline-warning btn-lg" style={{margin: 'auto'}}>I'm a New Guru </button>
-        {/* <button type="button" className="btn btn-outline-info btn-lg" style={{margin: 'auto'}}>Trends</button> */}
+    
+    <div className='bottom-container24'>
+      <button type="button" className="btn btn-primary btn-lg">I'm in Need of Guru Guidance</button>
+      <button type="button" className="btn btn-primary btn-lg" >I'm a New Guru </button> 
     </div>
 </div>
   )
